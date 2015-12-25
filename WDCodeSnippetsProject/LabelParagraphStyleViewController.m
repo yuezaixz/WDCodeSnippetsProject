c1d44bfd9ebca40d1d873c3de7422a446718e83a
yuezaixz <303178394@qq.com>
2015-12-25 14:54:38 +0800

    行间距代码段

--- WDCodeSnippetsProject/LabelParagraphStyleViewController.m
+++ WDCodeSnippetsProject/LabelParagraphStyleViewController.m
@@ -8,7 +8,11 @@
 
 #import "LabelParagraphStyleViewController.h"
 
+#define LINE_SPACING 10
+
 @interface LabelParagraphStyleViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *testLabel;
+@property (strong, nonatomic) NSMutableArray *textArray;
 
 @end
 
@@ -16,22 +20,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    self.textArray = [@[@"跑动跑鞋"] mutableCopy];
+}
+- (IBAction)addTextAction:(id)sender {
+    [self.textArray addObject:[NSString stringWithFormat:@"测试%ld",[self.textArray count]]];
+    
+    NSMutableParagraphStyle *paragraphStyle = [NSMutableParagraphStyle new];
+    paragraphStyle.lineSpacing = LINE_SPACING;
+    NSAttributedString *str = [[NSAttributedString alloc] initWithString:[self textContent] attributes:@
+                               
+                               {
+                               NSFontAttributeName: [UIFont systemFontOfSize:16],
+                               NSParagraphStyleAttributeName: paragraphStyle,
+                               }];
+    //有一个问题就是如果只有一行，那么下方还会有个空白的行间距
+    self.testLabel.attributedText = str;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSString *)textContent{
+    NSMutableString *text = [[NSMutableString alloc] init];
+    for (NSString *str in self.textArray) {
+        [text appendString:str];
+    }
+    return text;
 }
-*/
 
 @end
