e6064f4b1cf705dd1ecd0bcd741a833295074858
yuezaixz <303178394@qq.com>
2015-12-25 14:06:35 +0800

    Initial Commit

---
+++ WDCodeSnippetsProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WDCodeSnippetsProject
+//
+//  Created by 吴迪玮 on 15/12/25.
+//  Copyright © 2015年 DNT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WDCodeSnippetsProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WDCodeSnippetsProject
+//
+//  Created by 吴迪玮 on 15/12/25.
+//  Copyright © 2015年 DNT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
